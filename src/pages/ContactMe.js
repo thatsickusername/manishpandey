54352be80b883c040f150448b139d90177aa6f33
thatsickusername <60377461+thatsickusername@users.noreply.github.com>
2021-07-08 23:28:35 +0530

    mail to

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -9,7 +9,7 @@ function ContactMe() {
                 <div className="ContactMeTextWrapper">
                     <h2>Contact Me</h2>
                     <ul>
-                        <a href="emailaddress:manish1234pandeycrpf@gmail.com" target="blank"><li>@EmailMe</li></a>
+                        <a href="mailto:manish1234pandeycrpf@gmail.com" target="blank"><li>@EmailMe</li></a>
                         <a href="https://www.linkedin.com/in/manish-pandey-245b83194/" target="blank"><li>@LinkedIn</li></a>
                         <a href="https://github.com/thatsickusername" target="blank"><li>@GitHub</li></a>
                         <a href="contactnumber:+918882078095" target="blank"><li>@Contact No.</li></a>
