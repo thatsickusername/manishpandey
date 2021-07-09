b2bba9e925864dc6892b6d84c16a69fbe76cc44a
thatsickusername <60377461+thatsickusername@users.noreply.github.com>
2021-07-10 01:18:14 +0530

    resume updated

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -10,7 +10,7 @@ function AboutMe() {
                 <div className="AboutMeTextWrapper">
                     <h2>Little <br/> About Me</h2>
                     <h5>HI! My name is Manish Pandey. I design and develop interactive websites. I am currently in the second year of my BTech in Computer science and looking for good internship opportunities to headstart my career in the field. My aim is to visualize your ideas through my front end development skills, so if you have a project idea in mind and wanna work together feel free to get in touch.</h5>
-                    <a href="https://drive.google.com/file/d/1RaCd2P8HzXQ9RhHXUPDEQAwKwxrh1Urg/view?usp=sharing" target="blank"><h5 className="AboutMeMore">Download my resume</h5></a>
+                    <a href="https://drive.google.com/file/d/1e6xpe2nhZ2rDcOdZ2LRDrZNDoRH1_dtM/view?usp=sharing" target="blank"><h5 className="AboutMeMore">Download my resume</h5></a>
                 </div>
                 
                 <div className="AboutMeImageWrapper">
