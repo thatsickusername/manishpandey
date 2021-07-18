8cd6b0624b8ef215774edb3e3cacae439764da26
thatsickusername <60377461+thatsickusername@users.noreply.github.com>
2021-07-18 19:24:34 +0530

    Update HeroSection.js

--- src/pages/HeroSection.js
+++ src/pages/HeroSection.js
@@ -12,7 +12,7 @@ function HeroSection() {
             <div className="HeroContainer">
                 <div className="HeroTextWrapper">
                     <h1>MANISH PANDEY</h1>
-                    <h4>A VISUAL PORTFOLIO</h4>
+                    <h4>FRONTEND DEVELOPER</h4>
                 </div>
                 <div className="ScrollText">
                     <h4>SCROLL DOWN</h4>
